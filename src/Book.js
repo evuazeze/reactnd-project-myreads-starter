56555fd3dab90f0ada63c7ba8700d468a97a1cc5
Emmanuel Evuazeze <evuazeze.emmanuel@gmail.com>
2021-05-30 17:58:08 +0100

    change Book component to a pure component

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import * as BooksAPI from './BooksAPI';
 
-class Book extends Component {
+class Book extends PureComponent {
 
     changeShelf = event => {
         const book = this.props.book;
